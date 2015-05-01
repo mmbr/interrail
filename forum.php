858f5920cf3e51daac97d728366ea551d88532f6
mmbr <marianabriz06@hotmail.com>
2015-05-01 18:43:47 +0100

    Update forum.php

--- forum.php
+++ forum.php
@@ -1,7 +1,127 @@
 <?php include("header.php"); ?>
 
 
+	<div id="divForum">
 
+			
+  
+ 
+  <nav id="forum">
+
+
+      <form>
+      
+              <fieldset id="formInfo">
+                
+                <li class="forum_li">
+
+            	<div id="textbox">
+				  <a href = "forumUm.php">
+				  <p class="alignleft">Interrail Experience</p>
+				  <p class="alignright">234 threads</p>
+				  <p class="alignright">354 posts</p>
+				  </a>
+				</div>
+
+            	
+
+            	<svg height="7" width="2000" id="forumsvg">
+                <line x1="0" y1="0" x2="29.5%" y2="0" />
+                </svg> 
+
+                <h11>
+            	Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+				</h11>
+
+
+               </li>
+               
+
+
+                  <li class="forum_li">
+
+            	<div id="textbox">
+            	  <a href = "forumUm.php">
+				  <p class="alignleft"> funny stories </p>
+				  <p class="alignright">234 threads</p>
+				  <p class="alignright">354 posts</p>
+				 </a>
+				</div>
+
+            	
+
+            	<svg height="7" width="2000" id="forumsvg">
+                <line x1="0" y1="0" x2="29.5%" y2="0" />
+                </svg> 
+
+                <h11>
+            	It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using.
+				</h11>
+
+               </li>
+
+
+
+                  <li class="forum_li">
+
+            	<div id="textbox">
+            		<a href = "forumUm.php">
+				  <p class="alignleft"> the best cities </p>
+				  <p class="alignright">234 threads</p>
+				  <p class="alignright">354 posts</p>
+				 </a>
+				</div>
+
+            	
+
+            	<svg height="7" width="2000" id="forumsvg">
+                <line x1="0" y1="0" x2="29.5%" y2="0" />
+                </svg> 
+
+                <h11>
+            	There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly believable. If you are going to use a passage of Lorem Ipsum.
+				</h11>
+
+               </li>
+
+
+
+                  <li class="forum_li">
+
+            	<div id="textbox">
+            		<a href = "forumUm.php">
+				  <p class="alignleft"> suggestions </p>
+				  <p class="alignright">234 threads</p>
+				  <p class="alignright">354 posts</p>
+				  </a>
+				</div>
+
+            	
+
+            	<svg height="7" width="2000" id="forumsvg">
+                <line x1="0" y1="0" x2="29.5%" y2="0" />
+                </svg> 
+
+                <h11>
+            	Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old..
+				</h11>
+
+               </li>
+
+				              
+              </fieldset>
+
+
+      </form>
+      
+
+    </ul>
+   
+
+  </li>
+  </nav>
+
+  </div>
 
   </body>
 
