2627a769d1fac056f14659ffc3100e5bd1cc6e5a
mmbr <marianabriz06@hotmail.com>
2015-05-01 18:48:28 +0100

    Create forumDois.php

---
+++ forumDois.php
@@ -0,0 +1,143 @@
+<?php include("header.php"); ?>
+
+
+	<div id="divForum">
+  
+ 
+  <nav id="forum">
+
+
+    <div id="forum_space">  <a href = "forum.php"> <h13> [FORUM] </h13> <br> </a>   </div>
+    <h10>the best cities » Paris</h10>
+
+              <br>
+              <br>
+
+              <svg height="11" width="200" id="forumsvg">
+                <line x1="0" y1="0" x2="20" y2="0" />
+                </svg> 
+
+              <br>
+              <br>
+              <br>
+
+
+      <form>
+      
+              <fieldset id="formInfo">
+                
+                <li class="forum_li">
+
+            	<div id="textboxUm">
+      				  <a href = "forumUm.php">
+      				  <p class="alignleft" style="margin: 0 0 0">#1</p>
+      				  </a>
+      				</div>
+
+                <br>
+
+                <h11>
+                  	There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly believable. If you are going to use a passage of Lorem Ipsum.
+      				</h11>
+
+                <div id="forumUm_legenda">
+                <a href = "forumUm.php">
+                <p class="alignleft"> by _CrisFer </p>
+                <p class="alignleft"> 02 apr 2015 </p>
+                <p class="alignleft"> 05:00 pm </p>
+
+
+                <p class="alignright"> 0 COMMENTS </p>
+                <p class="alignright"> REPLY </p>
+               </a>
+              </div>
+
+
+
+
+               </li>
+               
+
+      <li class="forum_li">
+
+              <div id="textboxUm">
+                <a href = "forumUm.php">
+                <p class="alignleft" style="margin: 0 0 0">#2</p>
+                </a>
+              </div>
+
+                <br>
+
+                <h11>
+                    There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly believable. If you are going to use a passage of Lorem Ipsum.
+              </h11>
+
+                <div id="forumUm_legenda">
+                <a href = "forumUm.php">
+                <p class="alignleft"> by _CrisFer </p>
+                <p class="alignleft"> 02 apr 2015 </p>
+                <p class="alignleft"> 05:00 pm </p>
+
+
+                <p class="alignright"> 0 COMMENTS </p>
+                <p class="alignright"> REPLY </p>
+               </a>
+              </div>
+
+
+
+
+               </li>
+
+
+                       <li class="forum_li">
+
+              <div id="textboxUm">
+                <a href = "forumUm.php">
+                <p class="alignleft" style="margin: 0 0 0">#3</p>
+                </a>
+              </div>
+
+                <br>
+
+                <h11>
+                    There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly believable. If you are going to use a passage of Lorem Ipsum.
+              </h11>
+
+                <div id="forumUm_legenda">
+                <a href = "forumUm.php">
+                <p class="alignleft"> by _CrisFer </p>
+                <p class="alignleft"> 02 apr 2015 </p>
+                <p class="alignleft"> 05:00 pm </p>
+
+
+                <p class="alignright"> 0 COMMENTS </p>
+                <p class="alignright"> REPLY </p>
+               </a>
+              </div>
+
+
+
+
+               </li>
+
+            
+
+				              
+              </fieldset>
+
+
+      </form>
+      
+
+    </ul>
+   
+
+  </li>
+  </nav>
+
+  </div>
+
+  </body>
+
+</html>
